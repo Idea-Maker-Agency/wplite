9a461c9856621f5a255c9e32c3445347a458f0e5
Windex <windeloira07@gmail.com>
2024-06-28 06:29:54 +0800

    chore: vanilla wp theme

---
+++ theme/functions.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Define theme constants.
+ *
+ * @since 1.0.0
+ */
+define( 'THEME_DIR_PATH', get_template_directory() );
+define( 'THEME_DIR_URI', get_template_directory_uri() );
+
+require_once THEME_DIR_PATH . '/lib/init.php';
---
+++ theme/lib/init.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Init.
+ *
+ * @since 1.0.0
+ *
+ * @return void
+ */
+function wplite_init(): void {
+}
+
+wplite_init();

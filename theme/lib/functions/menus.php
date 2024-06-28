2cc7d9810381a8b9285624157ad0a8f25784ede7
Windex <windeloira07@gmail.com>
2024-06-28 08:13:40 +0800

    chore: register custom nav menu position
    feat: filter nav menu classes to use bs5 classes

---
+++ theme/lib/functions/menus.php
@@ -0,0 +1,77 @@
+<?php
+
+/**
+ * Register nav menus.
+ *
+ * @since 1.0.0
+ *
+ * @return void
+ */
+add_action( 'init', 'wplite_init_menu' );
+function wplite_init_menu (): void {
+  register_nav_menu( 'header-menu', __( 'Header Menu' ) );
+}
+
+/**
+ * Filters the HTML attributes applied to a menu's list item element.
+ *
+ * @since 1.0.0
+ *
+ * @param array     $atts       {
+ *    The HTML attributes applied to the menu item's `<li>` element, empty strings are ignored.
+ *    @type string    $class        HTML CSS class attribute.
+ *    @type string    $id           HTML id attribute.
+ * }
+ * @param WP_Post   $menu_item  The current menu item object.
+ * @param stdClass  $args       An object of wp_nav_menu() arguments.
+ * @param int       $depth      Depth of menu item. Used for padding.
+ *
+ * @return array    $atts
+ */
+add_filter( 'nav_menu_item_attributes', 'wplite_nav_menu_item_attributes', 10, 4 );
+function wplite_nav_menu_item_attributes (
+  array $atts,
+  WP_Post $menu_item,
+  stdClass $args,
+  int $depth
+): array {
+  $atts['class'] = 'nav-item';
+  $atts['id'] = 'nav-item-'. $menu_item->ID;
+
+  return $atts;
+}
+
+/**
+ * Filters the HTML attributes applied to a menu item's anchor element.
+ *
+ * @since 1.0.0
+ *
+ * @param array     $atts       {
+ *    The HTML attributes applied to the menu item's `<a>` element, empty strings are ignored.
+ *    @type string $title        Title attribute.
+ *    @type string $target       Target attribute.
+ *    @type string $rel          The rel attribute.
+ *    @type string $href         The href attribute.
+ *    @type string $aria-current The aria-current attribute.
+ * }
+ * @param WP_Post   $menu_item  The current menu item object.
+ * @param stdClass  $args       An object of wp_nav_menu() arguments.
+ * @param int       $depth      Depth of menu item. Used for padding.
+ *
+ * @return array    $atts
+ */
+add_filter( 'nav_menu_link_attributes', 'wplite_nav_menu_link_attributes', 10, 4 );
+function wplite_nav_menu_link_attributes (
+  array $atts,
+  WP_Post $menu_item,
+  stdClass $args,
+  int $depth
+): array {
+  $atts['class'] = 'nav-link';
+
+  if ( $menu_item->current ) :
+    $atts['class'] .= ' active';
+  endif;
+
+  return $atts;
+}
--- theme/lib/init.php
+++ theme/lib/init.php
@@ -10,6 +10,7 @@
 function wplite_init(): void {
   require_once THEME_DIR_PATH . '/lib/functions/styles.php';
   require_once THEME_DIR_PATH . '/lib/functions/scripts.php';
+  require_once THEME_DIR_PATH . '/lib/functions/menus.php';
 }
 
 wplite_init();

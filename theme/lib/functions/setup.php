e6e97a9e2cc7d9ed3a61a27342ef9da28e9a99bc
Windel <windeloira07@gmail.com>
2024-07-04 20:31:13 +0800

    chore: register 'featured-image' image size

--- theme/lib/functions/setup.php
+++ theme/lib/functions/setup.php
@@ -13,4 +13,5 @@ function wp_after_setup_theme(): void {
   add_theme_support( 'widgets' );
 
   add_image_size( 'card-image', 420, 320, [ 'center', 'center' ] );
+  add_image_size( 'featured-image', 640, 320, [ 'center', 'center' ] );
 }

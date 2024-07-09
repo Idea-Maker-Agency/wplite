4a715a8f12b0d2db45854436fbe7b40cba849f68
Windex <windeloira07@gmail.com>
2024-07-09 23:18:06 +0800

    chore: remove test content in front-page template

--- theme/front-page.php
+++ theme/front-page.php
@@ -11,12 +11,5 @@
 get_header();
 ?>
 
-<section class="py-5">
-  <div class="container">
-    <?php wplite_button( 'Test Button', 'success', 'sm', false, false, [ 'id' => 'test' ] ) ?>
-    <?php wplite_link( 'test link', 'https://google.com', 'success', 'sm', true, false, false, [ 'id' => 'test' ] ) ?>
-  </div>
-</section>
-
 <?php
 get_footer();

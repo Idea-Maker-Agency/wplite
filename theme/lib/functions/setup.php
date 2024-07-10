3e6578a1e3fb0b747d666671f1ac4fe521b65e34
Windex <windeloira07@gmail.com>
2024-07-10 22:22:33 +0800

    chore: override thumbnail, medium and large image sizes

--- theme/lib/functions/setup.php
+++ theme/lib/functions/setup.php
@@ -12,6 +12,10 @@ function wp_after_setup_theme(): void {
 	add_theme_support( 'post-thumbnails' );
   add_theme_support( 'widgets' );
 
+  add_image_size( 'thumbnail', get_option( 'thumbnail_size_w', 320 ), get_option( 'thumbnail_size_h', 240 ), true );
+  add_image_size( 'medium', get_option( 'medium_size_w', 640 ), get_option( 'medium_size_h', 480 ), true );
+  add_image_size( 'large', get_option( 'large_size_w', 1024 ), get_option( 'large_size_h', 768 ), true );
+
   add_image_size( 'card-image', 420, 320, [ 'center', 'center' ] );
   add_image_size( 'featured-image', 640, 320, [ 'center', 'center' ] );
 }

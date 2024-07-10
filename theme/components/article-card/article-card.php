ede14be17229154781a23b8d465e90083f42f322
Windex <windeloira07@gmail.com>
2024-07-10 14:10:33 +0800

    refactor: replace article card link with link component

--- theme/components/article-card/article-card.php
+++ theme/components/article-card/article-card.php
@@ -56,11 +56,12 @@ if ( is_null( $post ) ) return;
 
     <?php echo wpautop( get_the_excerpt( $post ) ) ?>
 
-    <a
-      href="<?php echo get_permalink( $post ) ?>"
-      class="card-link">
-      Read More
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right ms-2"><path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"/></svg>
-    </a>
+    <?php
+    wplite_link( 'Read More', get_permalink( $post ), 'primary', '', false, false, false, [
+      'class' => [ 'card-link' ],
+      'icon' => '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right ms-2"><path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"/></svg>',
+      'icon_pos' => 'end',
+    ] )
+    ?>
   </div>
 </article>

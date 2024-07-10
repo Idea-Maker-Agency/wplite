833f2e562c32d119604288901517bde536f2e6d1
Windex <windeloira07@gmail.com>
2024-07-10 22:08:05 +0800

    refactor: replace php components with custom code snippets

--- theme/components/article-card/article-card.php
+++ theme/components/article-card/article-card.php
@@ -9,7 +9,7 @@ if ( is_null( $post ) ) return;
 ?>
 
 <article
-  <?php post_class( 'card h-100', $post ) ?>
+  <?php post_class( 'article-card card h-100', $post ) ?>
   id="post-<?php echo $post->ID ?>"
   aria-label="<?php echo get_the_title( $post ) ?>"
   itemtype="https://schema.org/CreativeWork"
@@ -18,11 +18,19 @@ if ( is_null( $post ) ) return;
     href="<?php echo get_permalink( $post ) ?>"
     class="card-img-top">
     <?php if ( has_post_thumbnail( $post ) ) : ?>
-      <?php
-      wplite_image( get_the_post_thumbnail_url( $post->ID, 'card-image' ), 420, 320, [], [
-        'alt' => $post->post_title,
-      ] )
-      ?>
+      <img
+        src="<?php echo get_the_post_thumbnail_url( $post->ID, 'card-image' ) ?>"
+        width="420"
+        height="320"
+        srcset="
+          <?php echo get_the_post_thumbnail_url( $post->ID, 'card-image' ) ?> 1024w,
+          <?php echo get_the_post_thumbnail_url( $post->ID, 'thumbnail' ) ?> 320w
+        "
+        sizes="(min-width: 420px) 420px, 100vw"
+        alt="<?php echo $post->post_title ?>"
+        loading="lazy"
+        decoding="async"
+        class="img-fluid" />
     <?php else : ?>
       <svg width="100%" height="320" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Image cap" preserveAspectRatio="xMidYMid slice" focusable="false" style="text-anchor: middle;">
         <title>Placeholder</title>
@@ -56,12 +64,13 @@ if ( is_null( $post ) ) return;
 
     <?php echo wpautop( get_the_excerpt( $post ) ) ?>
 
-    <?php
-    wplite_link( 'Read More', get_permalink( $post ), 'primary', '', false, false, false, [
-      'class' => [ 'card-link' ],
-      'icon' => '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right ms-2"><path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"/></svg>',
-      'icon_pos' => 'end',
-    ] )
-    ?>
+    <a
+      href="<?php echo get_permalink( $post ) ?>"
+      class="card-link"
+      rel="noopener noreferrer"
+      alt="Read more about <?php echo get_the_title( $post ) ?>">
+      Read More
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right ms-2"><path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"/></svg>
+    </a>
   </div>
 </article>

9398921b0d870d0fcd295c3af11d5f4fffca84ae
Windex <windeloira07@gmail.com>
2024-07-11 11:37:14 +0800

    refactor: rename setup.php to settings.php

--- theme/lib/init.php
+++ theme/lib/init.php
@@ -8,7 +8,7 @@
  * @return void
  */
 function wplite_init(): void {
-  require_once THEME_DIR_PATH . '/lib/functions/setup.php';
+  require_once THEME_DIR_PATH . '/lib/functions/settings.php';
   require_once THEME_DIR_PATH . '/lib/functions/admin-header.php';
   require_once THEME_DIR_PATH . '/lib/functions/general-template.php';
   require_once THEME_DIR_PATH . '/lib/functions/components.php';

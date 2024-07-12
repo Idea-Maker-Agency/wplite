ac6a612d4e9f49f90f201a48eac9615896dcbfab
Windex <windeloira07@gmail.com>
2024-07-12 09:38:42 +0800

    chore: custom theme php helper functions

---
+++ theme/lib/functions/helpers.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Get webp asset image url.
+ *
+ * @param string    $name       The webp image file name.
+ * @param string    $path       If the webp image file is placed on different directory.
+ * @param array     $size       {
+ *  @param int        $width      The webp image file width.
+ *  @param int        $height     The webp image file height.
+ * }
+ *
+ * @return string
+ */
+function wplite_get_webp_url( string $name, string $path = '', array $size = [] ): string {
+  if ( !empty( $size ) ) :
+    [ $width, $height ] = $size;
+
+    $filename = $name .'-'. $width .'x'. $height .'.webp';
+  else :
+    $filename = $name .'.webp';
+  endif;
+
+  return THEME_DIR_URI .'/assets/lib/img'. $path .'/'. $filename;
+}
--- theme/lib/init.php
+++ theme/lib/init.php
@@ -8,6 +8,7 @@
  * @return void
  */
 function wplite_init(): void {
+  require_once THEME_DIR_PATH . '/lib/functions/helpers.php';
   require_once THEME_DIR_PATH . '/lib/functions/settings.php';
   require_once THEME_DIR_PATH . '/lib/functions/admin-header.php';
   require_once THEME_DIR_PATH . '/lib/functions/general-template.php';

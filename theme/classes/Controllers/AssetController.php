5aeb74ceeea005c19d2c0d5a0be37ab653d9921d
Windel <windeloira07@yahoo.com>
2025-07-21 16:28:22 +0800

    refactor(template-assets): move scss modules to template's dir

--- theme/classes/Controllers/AssetController.php
+++ theme/classes/Controllers/AssetController.php
@@ -167,8 +167,8 @@ class AssetController
       $slug = 'front-page';
     }
 
-    $path = get_theme_file_path("assets/lib/css/templates/{$slug}.css");
-    $uri  = get_theme_file_uri("assets/lib/css/templates/{$slug}.css");
+    $path = get_theme_file_path("templates/{$slug}/{$slug}.css");
+    $uri  = get_theme_file_uri("templates/{$slug}/{$slug}.css");
 
     if (! file_exists($path)) {
       return;
@@ -189,14 +189,20 @@ class AssetController
   {
     global $post;
 
-    $path = get_theme_file_path("templates/{$post->post_name}/{$post->post_name}.js");
-    $uri  = get_theme_file_uri("templates/{$post->post_name}/{$post->post_name}.js");
+    $slug = $post->post_name;
+
+    if (is_front_page()) {
+      $slug = 'front-page';
+    }
+
+    $path = get_theme_file_path("templates/{$slug}/{$slug}.js");
+    $uri  = get_theme_file_uri("templates/{$slug}/{$slug}.js");
 
     if (! file_exists($path)) {
       return;
     }
 
-    $handle  = "wplite-{$post->post_name}";
+    $handle  = "wplite-{$slug}";
     $version = filemtime($path);
 
     wp_enqueue_script($handle, $uri, [], $version, true);

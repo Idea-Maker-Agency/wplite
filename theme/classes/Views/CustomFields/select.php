8d5247790e08b3fcb036955abb6b62e678dcf4f1
Windel <windeloira07@yahoo.com>
2025-07-24 07:03:30 +0800

    feat(custom-fields): capability for select field to posts as options

--- theme/classes/Views/CustomFields/select.php
+++ theme/classes/Views/CustomFields/select.php
@@ -1,11 +1,28 @@
 <?php
 $post_id  = (int) $args['post_id']     ?? 0;
 $name     = $args['name']              ?? '';
+$multiple = $args['field']['multiple'] ?? false;
 $required = $args['field']['required'] ?? false;
 $options  = $args['field']['options']  ?? [];
+$source   = $args['field']['post_type_source'];
 
 if (! empty($args['parent_name'])) {
   $name = "{$args['parent_name']}_{$name}";
+}
+
+// Use post type as source of options
+if (! empty($source) && post_type_exists($source)) {
+  $posts = get_posts([
+    'post_type' => $source,
+    'posts_per_page' => -1,
+  ]);
+
+  $options = array_map(function ($post) {
+    return [
+      'label' => $post->post_title,
+      'value' => $post->ID,
+    ];
+  }, $posts);
 }
 
 $post = get_post($post_id);
@@ -15,7 +32,8 @@ $post = get_post($post_id);
   id="id_field_<?= $name ?>"
   name="<?= $name ?>"
   class="postbox"
-  <?= $required ? 'required' : ''?>>
+  <?= $multiple ? 'multiple' : '' ?>
+  <?= $required ? 'required' : '' ?>>
   <option disabled>Select option</option>
 
   <?php if (! empty($options)) { ?>

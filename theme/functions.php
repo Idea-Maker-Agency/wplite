65eb232f6b5e2e3f4af4b65df50338223c36e5d4
Windel <windeloira07@yahoo.com>
2025-08-01 11:11:02 +0800

    feat: upgrade to v1.0.1

--- theme/functions.php
+++ theme/functions.php
@@ -5,7 +5,7 @@
  *
  * @since 1.0.0
  */
-define('THEME_VERSION', '1.0.0');
+define('THEME_VERSION', '1.0.1');
 define('THEME_TEXT_DOMAIN', 'wplite');
 
 define('THEME_DIR_PATH', get_template_directory());

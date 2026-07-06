27e1a7a5a30180e80629497cd28c47b6e0d860f6
Windel <windel@ideamaker.agency>
2026-07-06 12:10:01 +0800

    chore: page templates functions module

--- theme/functions.php
+++ theme/functions.php
@@ -13,4 +13,5 @@ define('THEME_DIR_URI', get_template_directory_uri());
 
 require_once get_theme_file_path('vendor/autoload.php');
 
+require_once get_theme_file_path('lib/page-templates.php');
 require_once get_theme_file_path('lib/init.php');

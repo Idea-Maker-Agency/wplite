9c9b1f099da6ddc31f57191dfbb24c9ce33bf00b
Windel <windel@ideamaker.agency>
2026-07-09 13:15:09 +0800

    refactor: move init file to inc

--- theme/functions.php
+++ theme/functions.php
@@ -12,4 +12,4 @@ define('THEME_DIR_PATH', get_template_directory());
 define('THEME_DIR_URI', get_template_directory_uri());
 
 require_once THEME_DIR_PATH . '/vendor/autoload.php';
-require_once THEME_DIR_PATH . '/lib/init.php';
+require_once THEME_DIR_PATH . '/inc/init.php';

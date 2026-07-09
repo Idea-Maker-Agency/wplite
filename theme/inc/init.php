01a03dd6ff8c7c7ed9ff3d8bf2ce7d69435995d2
Windel <windel@ideamaker.agency>
2026-07-09 14:29:24 +0800

    refactor: replace SetupController with functional hooks

--- theme/inc/init.php
+++ theme/inc/init.php
@@ -1,9 +1,6 @@
 <?php
 
-use WPLite\Controllers\{
-  SetupController,
-  ContactForm7Controller
-};
+use WPLite\Controllers\ContactForm7Controller;
 use WPLite\Controllers\Admin\{
   ThemeCustomizerController,
   CustomFieldsRepeaterAJAXController,
@@ -17,13 +14,15 @@ use WPLite\Controllers\Form\{
 require_once THEME_DIR_PATH . '/inc/functions-templates.php';
 require_once THEME_DIR_PATH . '/inc/functions-page-templates.php';
 
+require_once THEME_DIR_PATH . '/inc/hooks-setup.php';
+require_once THEME_DIR_PATH . '/inc/hooks-formatting.php';
+require_once THEME_DIR_PATH . '/inc/hooks-script-loader.php';
+require_once THEME_DIR_PATH . '/inc/hooks-nav-menu.php';
 require_once THEME_DIR_PATH . '/inc/hooks-auth.php';
 require_once THEME_DIR_PATH . '/inc/hooks-general-template.php';
 require_once THEME_DIR_PATH . '/inc/hooks-templates.php';
 require_once THEME_DIR_PATH . '/inc/hooks-comment-template.php';
-require_once THEME_DIR_PATH . '/inc/hooks-nav-menu.php';
 require_once THEME_DIR_PATH . '/inc/hooks-page-templates.php';
-require_once THEME_DIR_PATH . '/inc/hooks-script-loader.php';
 
 /**
  * Init.
@@ -35,8 +34,6 @@ require_once THEME_DIR_PATH . '/inc/hooks-script-loader.php';
 add_action('after_setup_theme', 'wplite_init');
 function wplite_init()
 {
-  new SetupController();
-
   if (class_exists('WPCF7')) {
     ContactForm7Controller::init();
   }

cf2042326cae2dd632810de4cc87fa93373da18a
Windel <windel@ideamaker.agency>
2026-07-12 12:02:48 +0800

    fix: theme customizer restriction

--- theme/inc/admin/hooks-customizer.php
+++ theme/inc/admin/hooks-customizer.php
@@ -5,63 +5,68 @@
  *
  * @param \WP_Customize_Manager $manager
  */
-function wplite_customize_register(\WP_Customize_Manager $manager) {
+function wplite_customize_register(\WP_Customize_Manager $manager)
+{
+    if (! current_user_can('manage_options')) {
+        return;
+    }
+
     $file     = get_theme_file_path('/customizer.json');
     $contents = file_get_contents($file);
     $panels   = json_decode($contents, true);
 
     if (! empty($panels)) {
-      	foreach ($panels as $panel) {
-			$panel_id = "wplite_{$panel['panel']}";
+        foreach ($panels as $panel) {
+            $panel_id = "wplite_{$panel['panel']}";
 
-			$manager->add_panel($panel_id, [
-				'title'       => __($panel['title'], THEME_TEXT_DOMAIN),
-				'description' => __($panel['description'], THEME_TEXT_DOMAIN),
-				'priority'    => $panel['priority'] ?? 10,
-			]);
+            $manager->add_panel($panel_id, [
+                'title'       => __($panel['title'], THEME_TEXT_DOMAIN),
+                'description' => __($panel['description'], THEME_TEXT_DOMAIN),
+                'priority'    => $panel['priority'] ?? 10,
+            ]);
 
-			if (! empty($panel['sections'])) {
-				foreach ($panel['sections'] as $section) {
-					$section_id = "wplite_{$section['name']}";
+            if (! empty($panel['sections'])) {
+                foreach ($panel['sections'] as $section) {
+                    $section_id = "wplite_{$section['name']}";
 
-					$manager->add_section($section_id, [
-						'title' => __($section['title'], THEME_TEXT_DOMAIN),
-						'panel' => $panel_id,
-					]);
+                    $manager->add_section($section_id, [
+                        'title' => __($section['title'], THEME_TEXT_DOMAIN),
+                        'panel' => $panel_id,
+                    ]);
 
-					if (! empty($section['settings'])) {
-						foreach ($section['settings'] as $setting) {
-							$setting_id   = "{$section_id}_{$setting['name']}";
-							$setting_args = [
-								'label'       => __($setting['label'], THEME_TEXT_DOMAIN),
-								'description' => __($setting['description'] ?? '', THEME_TEXT_DOMAIN),
-								'section'     => $section_id,
-								'settings'    => $setting_id,
-								'type'        => $setting['type'],
-							];
+                    if (! empty($section['settings'])) {
+                        foreach ($section['settings'] as $setting) {
+                            $setting_id   = "{$section_id}_{$setting['name']}";
+                            $setting_args = [
+                                'label'       => __($setting['label'], THEME_TEXT_DOMAIN),
+                                'description' => __($setting['description'] ?? '', THEME_TEXT_DOMAIN),
+                                'section'     => $section_id,
+                                'settings'    => $setting_id,
+                                'type'        => $setting['type'],
+                            ];
 
-							if ('select' === $setting['type'] && isset($setting['source'])) {
-								$setting_args['choices'] = _wplite_resolve_select_choices($setting['source']);
-							}
+                            if ('select' === $setting['type'] && isset($setting['source'])) {
+                                $setting_args['choices'] = _wplite_resolve_select_choices($setting['source']);
+                            }
 
-							$manager->add_setting($setting_id, [
-								'sanitize_callback' => 'wp_kses_post',
-							]);
+                            $manager->add_setting($setting_id, [
+                                'sanitize_callback' => 'wp_filter_no_kses',
+                            ]);
 
-							if ('image' === $setting['type']) {
-								$manager->add_control(new \WP_Customize_Image_Control(
-									$manager,
-									$setting_id,
-									$setting_args
-								));
-							} else {
-								$manager->add_control($setting_id, $setting_args);
-							}
-						}
-					}
-				}
-			}
-      }
+                            if ('image' === $setting['type']) {
+                                $manager->add_control(new \WP_Customize_Image_Control(
+                                    $manager,
+                                    $setting_id,
+                                    $setting_args
+                                ));
+                            } else {
+                                $manager->add_control($setting_id, $setting_args);
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 add_action('customize_register', 'wplite_customize_register');
@@ -72,17 +77,18 @@ add_action('customize_register', 'wplite_customize_register');
  * @param  string $source
  * @return array
  */
-function _wplite_resolve_select_choices(string $source): array {
+function _wplite_resolve_select_choices(string $source): array
+{
     $choices = [
       null => __('Select option', THEME_TEXT_DOMAIN),
     ];
 
     if ($source === 'wp-nav-menus') {
-		$menus = wp_get_nav_menus();
+        $menus = wp_get_nav_menus();
 
-		foreach ($menus as $menu) {
-			$choices[$menu->term_id] = $menu->name;
-		}
+        foreach ($menus as $menu) {
+            $choices[$menu->term_id] = $menu->name;
+        }
     }
 
     return $choices;
